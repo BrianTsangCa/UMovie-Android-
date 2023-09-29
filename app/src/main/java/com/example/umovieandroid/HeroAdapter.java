fb122992e7e4116e63d4b69d800517f5afef2368
300360289 <tyh1004@gmail.com>
2023-09-29 16:16:37 -0700

    hero section

---
+++ app/src/main/java/com/example/umovieandroid/HeroAdapter.java
@@ -0,0 +1,64 @@
+package com.example.umovieandroid;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.ViewHolder> {
+    Context context;
+    List<String> imgList;
+    final String img = "https://image.tmdb.org/t/p/w500";
+
+    public HeroAdapter() {
+    }
+
+    public HeroAdapter(Context context, List<String> imgList) {
+        this.context = context;
+        this.imgList = imgList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.carousel_layout, parent, false);
+        ViewHolder holder = new ViewHolder(itemView);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ImageView imageView = holder.carousel_image_view;
+        Picasso.get().load(img + imgList.get(position)).into(imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imgList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView carousel_image_view;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            carousel_image_view = itemView.findViewById(R.id.carousel_image_view);
+        }
+    }
+}
--- app/src/main/java/com/example/umovieandroid/MainActivity.java
+++ app/src/main/java/com/example/umovieandroid/MainActivity.java
@@ -1,19 +1,45 @@
 package com.example.umovieandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    RecyclerView carousel_recycler_view;
+    List<String> imglist = new ArrayList<>();
+    HeroAdapter heroAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        carousel_recycler_view = findViewById(R.id.carousel_recycler_view);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
@@ -32,5 +58,42 @@ public class MainActivity extends AppCompatActivity {
             // FirebaseUser.getIdToken() instead.
             String uid = user.getUid();
         }
+
+        String url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray results = response.getJSONArray("results");
+                            for (int i = 0; i < results.length(); i++) {
+                                JSONObject Data = results.getJSONObject(i);
+                                imglist.add(Data.getString("poster_path"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        heroAdapter = new HeroAdapter(MainActivity.this, imglist);
+                        carousel_recycler_view.setAdapter(heroAdapter);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.d("error", "Error: " + error.getMessage());
+                    }
+                }
+
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // Add the Authorization header with the Bearer token
+                headers.put("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjM2U5NGNjMzk3ZTkyYTFlMjdkOWM2YmE2NDAyYWVjMSIsInN1YiI6IjY1MDBmNjg0ZWZlYTdhMDExYWI4MzZlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FyDIEJK4BE6pY5GqHJQM0EOCbnii7XmB8NjUy9vonnQ");
+                return headers;
+            }
+        };
+        requestQueue.add(jsonObjectRequest);
     }
 }
