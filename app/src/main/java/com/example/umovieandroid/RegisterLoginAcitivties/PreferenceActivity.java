123b9f1c803bc5b63b84f2566fbe687e0462d46f
300360289 <tyh1004@gmail.com>
2023-10-04 14:03:42 -0700

    Preference Page.

---
+++ app/src/main/java/com/example/umovieandroid/RegisterLoginAcitivties/PreferenceActivity.java
@@ -0,0 +1,16 @@
+package com.example.umovieandroid.RegisterLoginAcitivties;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.umovieandroid.R;
+
+public class PreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference);
+    }
+}
